49595801f5fcfd1f1917b29f7c75bd203a27e997
raju2712 <rajuramikpm@gmail.com>
2025-04-24 18:21:57 +0530

    jk

--- Java_Programs/src/Assignment/RemoveDuplicateUsingString.java
+++ Java_Programs/src/Assignment/RemoveDuplicateUsingString.java
@@ -7,6 +7,7 @@ public class RemoveDuplicateUsingString {
 	public static void main(String[] args) {
 		LinkedHashSet set = new LinkedHashSet();
 		String s = "My name is My name is john";
+		
 		for(int i=0; i<s.length(); i++) {
 			set.add(s.charAt(i));
 		}
--- Java_Programs/src/Assignment/UniqueUsingList.java
+++ Java_Programs/src/Assignment/UniqueUsingList.java
@@ -7,27 +7,27 @@ import java.util.Map;
 
 public class UniqueUsingList {
 
-	  public static void main(String[] args) {
-	        // Using a List instead of an array
-	        List<Integer> list = Arrays.asList(1, 2, 3, 2, 3, 2, 3);
+	public static void main(String[] args) {
+		// Using a List instead of an array
+		List<Integer> list = Arrays.asList(1, 2, 3, 5, 3, 2, 4, 4);
 
-	        // Use LinkedHashMap to maintain insertion order
-	        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+		// Use LinkedHashMap to maintain insertion order
+		LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
 
-	        for (int num : list) {
-	            if (map.containsKey(num)) {
-	                map.put(num, map.get(num) + 1);
-	            } else {
-	                map.put(num, 1);
-	            }
-	        }
+		for (int num : list) {
+			if (map.containsKey(num)) {
+				map.put(num, map.get(num) + 1);
+			} else {
+				map.put(num, 1);
+			}
+		}
 
-	        // Print only numbers that occur once
-	        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-	            if (entry.getValue() == 1) {
-	                System.out.println(entry.getKey() + " : " + entry.getValue());
-	            }
-	        }
-	    }
+		// Print only numbers that occur once
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			if (entry.getValue() == 1) {
+				System.out.println(entry.getKey() + " Presents " + entry.getValue() + " time");
+			}
+		}
+	}
 
 }
