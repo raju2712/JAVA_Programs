76b4e38ab8a586c9e201f2b09aa2a1e4ff46c1f8
raju2712 <rajuramikpm@gmail.com>
2025-04-28 09:18:30 +0530

    28/04/2025

---
+++ Java_Programs/src/Assignment/aaa.java
@@ -0,0 +1,22 @@
+package Assignment;
+
+import java.util.ArrayList;
+
+public class aaa {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer>a=new ArrayList<Integer>(); 
+		  System.out.println(a.isEmpty()); //true 
+		  a.add(2); 
+		  a.add(3);  
+		  a.add(202); 
+		  a.add(69);  
+		  a.add(null); 
+		 
+		  for (Integer i : a) { 
+		   System.out.println(i); 
+		  } 
+	}
+
+}
